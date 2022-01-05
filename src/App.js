18ae2698631bc1be4209301f00e8e18086d9d466
vinishvijay <v4vinuzz@gmail.com>
2022-01-04 20:07:08 -0500

    added header component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
-function App() {
-  return <div className="App">Welcome to the world of entertainment!</div>;
-}
+import React from 'react';
+import Header from './Header';
+
+const App = () => {
+  const handleNameChange = () => {
+    const names = ['Bob', 'Kevin', 'Peter', 'Jack', 'Ashwin'];
+    const num = Math.floor(Math.random() * 3);
+    return names[num];
+  };
+
+  return (
+    <>
+      <Header />
+      <p>Welcome to the world {handleNameChange()}</p>
+      <p>{`5 + 6 : ${5 + 6}`}</p>
+      <p>Array : {[1, 2, 3]}</p>
+    </>
+  );
+};
 
 export default App;
